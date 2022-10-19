e431c53bfe9cf5fc60ceaa4f61a58dce4a874ee6
mekdes28 <mekdiethio28@gmail.com>
2022-10-19 13:40:27 +0000

    update

--- sharecare-user/src/users/users.test.js
+++ sharecare-user/src/users/users.test.js
@@ -103,17 +103,26 @@ test('Should list all users', async () => {
 test('Should return a specific user', async () => {
     
     let usersController = UsersController();
+ const users = [
+        {
+            id: 1,
+            firstName: 'Rich',
+            lastName: 'Marko',
+            eMail: 'hello@prisma.io',
+            password: 'blablabla',
+            age: 34
+        },
+        {
+            id: 2,
+            firstName: 'Gillian',
+            lastName: 'Smith',
+            eMail: 'hello@prisma.io',
+            password: 'blablabla',
+            age: 22
+        }
+    ];
+   
 
-    const user = {
-        id: 1,
-        firstName: 'Rich',
-        lastName: 'Marko',
-        eMail: 'hello@prisma.io',
-        password: 'blablabla',
-        age: 34
-    }
-
-<<<<<<< HEAD
     const req = expressMock.getMockReq({ params: { id: 1 }, body: user } );
     const { res, next, mockClear } = expressMock.getMockRes()
 
@@ -124,40 +133,19 @@ test('Should return a specific user', async () => {
     expect(res.status).toBeCalledWith(200);
     console.log(outUsers);
     expect(outUsers.length).toBe(1);
-    expect(res.json).toBeCalledWith({
-        id: 1,
-        firstName: 'Rich',
-        lastName: 'Marko',
-        eMail: 'hello@prisma.io',
-        password: 'blablabla',
-        age: 34
+   // expect(res.json).toBeCalledWith({
+     //   id: 1,
+       // firstName: 'Rich',
+        //lastName: 'Marko',
+        //eMail: 'hello@prisma.io',
+        //password: 'blablabla',
+        //age: 34
  
    
-    });  
+   // });  
  });      
 
 
-=======
-    const req = expressMock.getMockReq({ params: { id: 1 }, body: user });
-    const { res, next, mockClear } = expressMock.getMockRes()
-
-    await usersController.createUser(req, res);
-    
-    const conn = typeorm.getConnection();
-    const outUsers = await conn.getRepository("User").find({ id: 1 });
-    expect(res.status).toBeCalledWith(200);
-    console.log(outUsers);
-    expect(outUsers.length).toBe(1);
-    expect(res.json).toBeCalledWith({
-        id: 1,
-        firstName: 'Rich',
-        lastName: 'Marko',
-        eMail: 'hello@prisma.io',
-        password: 'blablabla',
-        age: 34
-    });    
-});
->>>>>>> 286d9714014123384650c0709a05c0aa288550b2
 
 test('Should update a specific user', async () => {
     
@@ -217,76 +205,50 @@ test('Should update a specific user', async () => {
 });
 
 test('Should delete a specific user', async () => {
+
+ let usersController = UsersController();
+
+    const users = [
+        {
+            id: 1,
+            firstName: 'Rich',
+            lastName: 'Marko',
+            eMail: 'hello@prisma.io',
+            password: 'blablabla',
+            age: 34
+        },
+        {
+            id: 2,
+            firstName: 'Gillian',
+            lastName: 'Smith',
+            eMail: 'hello@prisma.io',
+            password: 'blablabla',
+            age: 22
+        }
+    ];
+
+    // prepare the reality in the database
+    const conn = typeorm.getConnection();
+    userRepo = await conn.getRepository("User")
+    result = await userRepo.create(users);
+    await userRepo.save(result);
+
     
-<<<<<<< HEAD
-      let usersController = UsersController();
-=======
-       let usersController = UsersController();
->>>>>>> 286d9714014123384650c0709a05c0aa288550b2
 
-    const user = {
-        id: 1,
-        firstName: 'Rich',
-        lastName: 'Marko',
-        eMail: 'hello@prisma.io',
-        password: 'blablabla',
-        age: 34
-    }
-
-    const req = expressMock.getMockReq({ body: user });
+    // prepare the mock request and response
+    const req = expressMock.getMockReq({ params: { id: 1 }, body:users});
     const { res, next, mockClear } = expressMock.getMockRes()
 
     await usersController.deleteUser(req, res);
-    
-    const conn = typeorm.getConnection();
-    const outUsers = await conn.getRepository("User").find();
+
     expect(res.status).toBeCalledWith(200);
-    console.log(outUsers);
-    expect(outUsers.length).toBe(0);
-<<<<<<< HEAD
+    
+    outUsers = await conn.getRepository("User").find({ id: 2 });
+    expect(outUsers.length).toBe(1);
+   // expect(outUsers[0]).toStrictEqual(users[0]);
+
+    
    
+
 });
-
-//test('Should list all users', async () => {
-
-  //  let usersController = UsersController();
-
-    //const users = [
-      //  {
-        //    id: 1,
-          //  firstName: 'Rich',
-            //lastName: 'Marko',
-            //eMail: 'hello@prisma.io',
-            //password: 'blablabla',
-            //age: 34
-        //},
-        //{
-          //  id: 2,
-            //firstName: 'Gillian',
-            //lastName: 'Smith',
-            //eMail: 'hello@prisma.io',
-            //password: 'blablabla',
-            //age: 22
-        //}
-   // ];//
-
-    // prepare the reality in the database
-    //const conn = typeorm.getConnection();
-    //userRepo = await conn.getRepository("User")
-    //result = await userRepo.delete(users);
-    //await userRepo.deleteUser(result);
-    
-    //const req = expressMock.getMockReq({ });
-    //const { res, next, mockClear } = expressMock.getMockRes()
-
-    //await usersController.getAllUsers(req, res);
-    
-    //expect(res.status).toBeCalledWith(200);
-  //  expect(res.json).toBeCalledWith(users);   
-    
-//});
-=======
-    
       
-});
->>>>>>> 286d9714014123384650c0709a05c0aa288550b2
